bb0dc50f2caadf80029401356dc0f69f26caf203
Sebastian <s.scatularo@globant.com>
2016-12-15 17:31:22 -0300

    Initial commit

---
+++ src/main/java/ar/com/sebastianscatularo/examples/JwtSecurityExampleApplication.java
@@ -0,0 +1,12 @@
+package ar.com.sebastianscatularo.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/ar/com/sebastianscatularo/examples/JwtSecurityExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.com.sebastianscatularo.examples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtSecurityExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
